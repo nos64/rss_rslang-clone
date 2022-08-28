2a6a67c1439ad3f22bf2f35beee7b29db56d8097
nos64 <manosov1984@gmail.com>
2022-08-29 01:11:28 +0400

    feat: update branch (auth)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,17 @@ import './styles/index.scss';
 import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './components/App/App';
+import Context from './context';
+import store from './store';
+
+const ContextWrapper = () => {
+  const contextValue = React.useMemo(() => ({ store }), [store]);
+  return (
+    <Context.Provider value={contextValue}>
+      <App />
+    </Context.Provider>
+  );
+};
 
 const div = document.createElement('div');
 div.id = 'root';
@@ -12,7 +23,7 @@ const root = createRoot(div);
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <ContextWrapper />
     </HashRouter>
   </React.StrictMode>
 );
